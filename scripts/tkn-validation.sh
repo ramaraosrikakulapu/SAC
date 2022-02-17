94d91e4fb542152c6c9ba0fed1292e1070d3888c
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2022-02-17 10:29:05 -0800

    Create tkn-validation.sh

---
+++ scripts/tkn-validation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#  Copyright (c) 2019 General Electric Company. All rights reserved.
+#
+#  The copyright to the computer software herein is the property of
+#  General Electric Company. The software may be used and/or copied only
+#  with the written permission of General Electric Company or in accordance
+#  with the terms and conditions stipulated in the agreement/contract
+#  under which the software has been supplied.
+#
