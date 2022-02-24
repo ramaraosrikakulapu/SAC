52a8fda1963c04e3d7a4818428a5104760478c89
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2022-02-23 22:38:30 -0800

    Update tkn-validation.sh

--- scripts/tkn-validation.sh
+++ scripts/tkn-validation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#  Copyright (c) 2019 General Electric Company. All rights reserved.
+#  Copyright (c) 2020 General Electric Company. All rights reserved.
 #
 #  The copyright to the computer software herein is the property of
 #  General Electric Company. The software may be used and/or copied only
@@ -8,3 +8,21 @@
 #  with the terms and conditions stipulated in the agreement/contract
 #  under which the software has been supplied.
 #
+#  Author: apolo.yasuda@ge.com; apolo.yasuda.ge@gmail.com
+
+
+#functional interface to be implemented
+# function name: int_a
+#
+# param1: a base64 string of a custom json object (custom-obj) from script api
+# param2: a base64 string of a custom json object during sac instantiation. format: {\"region1-userpool1\":[\"service1\":\"service2\"],\"region1-userpool2\":[\"service1\":\"service2\"]}
+# 
+# return: stdout. 
+# i.e. printf "%s" "{\"decision\":\"PERMIT\"}";return 0;
+# i.e. printf "%s" "{\"error\":\"error from aws token verification.\"}";return -1;
+# ex: int_a "$EXEC_DAT" "$EXEC_MAP"
+ 
+function int_a() {
+  printf "{\"hello2\":\"world2\",\"dataFromrRequest\":%s,\"appParams\":%s}" "$1" "$2"
+  exit 0
+}
